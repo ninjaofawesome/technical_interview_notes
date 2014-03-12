d9c82f2b03747cd056ab510afec46270a24d01e2
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-12 10:24:05 -0400

    had fun with things

--- test1.js
+++ test1.js
@@ -1,3 +1,5 @@
+//from Interview Cake
+
 var threatLevel = 1;
 
 function inspireFear(threatLevel){
@@ -14,3 +16,23 @@ to 101, just add in a line that says
 "return threatLevel" as now threatLevel 1 will
 have the value of 1 added and assigned to 100.  
 Boom.)*/
+
+var myThings = ['black coffee', 'rollerball pen']
+
+function addToMyThings(myThings){
+	myThings.push('kinda mushy fuji apple');
+	myThings.push('music playlist');
+	//takes off the last thing in the array
+	myThings.pop();
+	//adds one thing to the first position, removes 0
+	myThings.splice(1, 0, 'sunny disposition');
+	//removes the third item in the array, adds this.
+	myThings.splice(2, 1, 'giant salad');
+}
+addToMyThings(myThings);
+console.log(myThings);
+
+/* This is an example of adding things to an 
+object (the array) by reference, then I just
+started having fun with messing around with the
+array in general. */
